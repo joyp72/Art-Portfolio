e14c560eef40dfef06955fdc42abc3fb50ba3798
Joy Patel <joyp72@gmail.com>
2022-05-27 02:10:35 -0400

    added mid-box-2 and reveal effect on text

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ let topMid = document.getElementById("top-mid");
 
 let leftIcon = document.getElementById("header-left-logo-button");
 let leftName = document.getElementById("header-left-name-button");
+let barButton = document.getElementById("header-right-bar-button");
+let sidebar = document.getElementById("sidebar");
+
+let topBox2 = document.getElementById("top-box2");
+let miniBox1 = document.getElementById("minibox-1");
+let miniBox2 = document.getElementById("minibox-2");
+let miniBox3 = document.getElementById("minibox-3");
 
 let midPainting1 = document.getElementById("mid-painting-1");
 let midPainting2 = document.getElementById("mid-painting-2");
 let midPainting3 = document.getElementById("mid-painting-3");
 
-let barButton = document.getElementById("header-right-bar-button");
-let sidebar = document.getElementById("sidebar");
+let footer = document.getElementById("footer");
 
 leftIcon.onmouseover = fadeLeftButtons;
 leftIcon.onmouseout = unFadeLeftButtons;
@@ -18,18 +24,38 @@ leftName.onmouseout = unFadeLeftButtons;
 
 let offset = 0;
 let sidebarToggle = 0;
+let revealed = 0;
 
 window.addEventListener("scroll", function () {
     offset = this.window.scrollY * 0.01;
     // console.log(offset);
+    reveal();
     adjustHeader();
     adjustPainting1();
     adjustPainting2();
     adjustPainting3();
 });
 
+function reveal() {
+    if (revealed == 0 && offset > 2) {
+        topBox2.style.transform = "translate(0)";
+        topBox2.style.opacity = "1";
+
+        miniBox1.style.transform = "translate(0)";
+        miniBox1.style.opacity = "1";
+
+        miniBox2.style.transform = "translate(0)";
+        miniBox2.style.opacity = "1";
+
+        miniBox3.style.transform = "translate(0)";
+        miniBox3.style.opacity = "1";
+
+        revealed = 1;
+    }
+}
+
 function adjustPainting1() {
-    if (offset > 6) {
+    if (offset > 6 && offset < 10) {
         let style = getComputedStyle(midPainting1);
         let cur = style.top;
         cur = cur.slice(0, 2);
@@ -92,4 +118,11 @@ function adjustHeader() {
     } else {
         topMid.style.opacity = "0";
     }
+}
+
+function scrollToFooter() {
+    footer.scrollIntoView();
+    barButton.style.transform = "rotate(0deg)"
+    sidebar.style.transform = "translate(0px)";
+    sidebarToggle = 0;
 }
